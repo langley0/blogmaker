562079a38b2f33ee4510fc0722169c0f1ccc792a
langley <langley0@naver.com>
2020-09-28 17:37:52 +0900

    FIX: 개발 서버에서 폴더위치 하드코딩 되어 있던것을 수정

--- src/devserver.ts
+++ src/devserver.ts
@@ -4,15 +4,14 @@ import mainpage from './mainpage';
 import post from './post';
 
 const app = express();
-app.use(express.static('public'));
+const root = '../langley0@github.io';
 
 app.get('/', (req, res) => {
-  res.send(mainpage('public'));
+  res.send(mainpage(root));
 });
 
 app.get('/:post', (req, res, next) => {
   try {
-    const root = 'public';
     const srcDir = req.params.post;
 
     // 포스트를 읽는다
@@ -22,8 +21,7 @@ app.get('/:post', (req, res, next) => {
   }
 });
 
-app.use(express.static('public'));
-
+app.use(express.static(root));
 app.listen(3000, () => {
   log('dev web server started at port 3000');
 });
